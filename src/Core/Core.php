933aed789a865226e26f5379abd702afb6df281d
fabioaraujob2b <fabioaraujob2b@gmail.com>
2024-07-12 17:45:36 -0300

    Configuracoes iniciais

---
+++ src/Core/Core.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+
+class Core 
+{
+    public static function dispatch(array $routes){
+        $url = '/';
+        
+        isset($_GET['url']) && $url .= $_GET['url'];
+
+        foreach ($routes as $route){
+            $pattern = '#^'.preg_replace('/{id}/', '([\w-]+)', $route['path']).'$#';
+
+            if(preg_match($pattern, $url, $matches)) {
+                print_r($matches);
+            }
+        }
+    }
+}
---
+++ src/routes/main.php
@@ -0,0 +1,5 @@
+<?php
+use App\Http\Route;
+
+Route::get('/', 'HomeController@index');
+Route::get('/about/{id}', 'HomeController@index');
